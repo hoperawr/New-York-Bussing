eb16b6a92f7fa2b75b638b67e67386cb97a345a9
Hope Leong <hopeleong@gmail.com>
2024-04-16 16:54:53 +0800

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div style={{position:"absolute",top:"0",height:"50px",backgroundColor:"#98F5F9", width:"100vw",margin:"0",padding:"0",left:"0"}}>
+      This is a Header
+    </div>
+  )
+}
+
+export default Header
